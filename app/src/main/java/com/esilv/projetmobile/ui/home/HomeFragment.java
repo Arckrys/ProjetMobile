8443cfda83290f6139ea4827c0dc6f2cdea9ceba
Jean COUTURON <jean.couturon@edu.devinci.fr>
2020-03-22 15:00:40 +0100

    Revert "Merge branch 'master' of https://github.com/Arckrys/ProjetMobile"
    This reverts commit 2701af561dcb5ca4003c83d0243fec60d5197c6b, reversing
    changes made to 489ee3b0128bde3d937087d2eff64d86d6e04e37.

--- app/src/main/java/com/esilv/projetmobile/ui/home/HomeFragment.java
+++ app/src/main/java/com/esilv/projetmobile/ui/home/HomeFragment.java
@@ -14,7 +14,6 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -23,9 +22,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.esilv.projetmobile.KitsuService;
 import com.esilv.projetmobile.R;
-import com.esilv.projetmobile.ui.search.SearchFragment;
-import com.esilv.projetmobile.ui.search.SearchViewModel;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.gson.annotations.SerializedName;
 
 import java.io.InputStream;
@@ -153,13 +149,7 @@ public class HomeFragment extends Fragment {
 
     }
 
-    private void swapFragment(){
-        SearchFragment newSearchFragment = new SearchFragment();
-        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.nav_view, newSearchFragment);
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.commit();
-    }
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
@@ -169,13 +159,6 @@ public class HomeFragment extends Fragment {
         loadManga(recyclerViewManga);
         RecyclerView recyclerViewAnime = root.findViewById(R.id.recyclerViewAnime);
         loadAnime(recyclerViewAnime);
-        FloatingActionButton searchButton = root.findViewById(R.id.searchButton);
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                swapFragment();
-            }
-        });
         return root;
     }
 }
